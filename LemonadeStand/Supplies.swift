e8237d47a304a728f8d266ed64747dd9d42d9acb
Koen Hendriks <koenhendriks89@gmail.com>
2014-11-09 14:13:08 +0100

    Added VC connections and supplies struct

---
+++ LemonadeStand/Supplies.swift
@@ -0,0 +1,21 @@
+//
+//  Supplies.swift
+//  LemonadeStand
+//
+//  Created by Koen Hendriks on 09/11/14.
+//  Copyright (c) 2014 Koen Hendriks. All rights reserved.
+//
+
+import Foundation
+
+struct supplies {
+    var money = 0
+    var lemons = 0
+    var iceCubes = 0
+    
+    init(aMoney:Int, aLemons:Int, aiceCubes:Int){
+        money = aMoney
+        lemons = aLemons
+        iceCubes = aiceCubes
+    }
+}
--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var fundsLabel: UILabel!
+    @IBOutlet weak var lemonsLabel: UILabel!
+    @IBOutlet weak var iceCubesLabel: UILabel!
+    
+    @IBOutlet weak var addedLemonsLabel: UILabel!
+    @IBOutlet weak var addedIceCubesLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: IBActions - Start Day Button
+    
+    @IBAction func startDayButtonPressed(sender: AnyObject) {
+    }
+    
+    
+    // MARK: IBActions - Purchasing
+    
+    @IBAction func purchaseLemonsButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func unpurchaseLemonsButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func purchaseIceCubesButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func unpurchaseIceCubesButtonPressed(sender: AnyObject) {
+    }
+    
+    // MARK: IBActions - Mixing
+    
+    @IBAction func addLemonsButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func removeLemonsButtonPressed(sender: AnyObject) {
+    }
 
-
+    @IBAction func addIceCubesButtonPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func removeIceCubesButtonPressed(sender: AnyObject) {
+    }
+    
+    
 }
+
+
+
+
 
